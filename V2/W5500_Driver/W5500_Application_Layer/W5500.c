3d3d7528fcaa04077968a036ceb1104341e42aed
embeddedalpha <kunalsalvi63@gmail.com>
2024-10-29 22:21:21 -0500

    Updated Application Layer

--- V2/W5500_Driver/W5500_Application_Layer/W5500.c
+++ V2/W5500_Driver/W5500_Application_Layer/W5500.c
@@ -8,14 +8,25 @@
 
 #include "W5500.h"
 
-
-
-bool W5500_UDP_Send_Data(uint8_t socket_number, uint8_t *data[], unit16_t length)
+bool W5500_UDP_Setup_Socket(uint8_t socket_number, uint8_t *d)
 {
 
 }
+
+bool W5500_UDP_Send_Data(uint8_t socket_number, uint8_t *data[], unit16_t length)
+{
+	// open socket
+	// fill destination ip and port
+	// fill buffer
+	// send data to socket
+	// close socket
+}
 bool W5500_UDP_Receive_Data(uint8_t socket_number, uint8_t *data[], unit16_t *length)
 {
-
+	// open socket
+	// fill destination ip and port
+	// fill buffer
+	// send data to socket
+	// close socket
 }
 
--- V2/W5500_Driver/W5500_Application_Layer/W5500.h
+++ V2/W5500_Driver/W5500_Application_Layer/W5500.h
@@ -12,6 +12,7 @@
 #include "W5500_Middleware/W5500_MWL.h"
 
 
+
 bool W5500_UDP_Send_Data(uint8_t socket_number, uint8_t *data[], unit16_t length);
 bool W5500_UDP_Receive_Data(uint8_t socket_number, uint8_t *data[], unit16_t *length);
 
